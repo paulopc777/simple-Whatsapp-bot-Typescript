c6d99f40a9a41afc2b7fd87632fc3c22aaa367b0
Paulo Cesar <62563753+paulopc777@users.noreply.github.com>
2023-12-17 13:55:37 -0300

    Update Cors tho express

--- src/TS/server.ts
+++ src/TS/server.ts
@@ -1,11 +1,22 @@
 
 var express = require('express');
 var app = express();
-import client  from './Bot/Zap';
+import client from './Bot/Zap';
 import { GravarAltomacao } from './Automation/AutoResponse';
 import path from 'node:path';
+var cors = require('cors')
+
 
 app.use(express.static(path.join(__dirname, '../../app')));
+
+const corsOptions = {
+    origin: 'https://bot-service-nqh5.onrender.com/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions))
 
 app.get('/', (req: any, res: any) => {
     res.send({ 'Clinet': 'Read' });
